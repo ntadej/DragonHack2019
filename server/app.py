e45ca1dbd8c1c8acb17445d6f26560cdc1084d91
Tadej Novak <tadej@tano.si>
2019-05-18 16:08:54 +0200

    Initial blockchain API

---
+++ server/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+from blockchain.api import blueprint as blockchain
+
+app = Flask(__name__)
+app.register_blueprint(blockchain, url_prefix='/api/blockchain')
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
+    args = parser.parse_args()
+    port = args.port
+
+    app.run(host='0.0.0.0', port=port)
