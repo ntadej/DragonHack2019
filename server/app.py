71a7f0cc4e4f9b9cc6f4824f679763e7ac4aed77
Ziga Brencic <ziga.brencic@desy.de>
2019-05-19 11:40:03 +0200

    adding blockchain to the robot object

--- server/app.py
+++ server/app.py
@@ -2,8 +2,10 @@ from flask import Flask
 from flask_cors import CORS
 from flask_sse import sse
 
-from blockchain.api import blueprint as blockchain
+from blockchain.api import blueprint, blockchain
 from robot.api import blueprint as robot
+
+robot.blockchain = blockchain
 
 app = Flask(__name__, static_url_path='/static', static_folder='../static/public')
 
@@ -11,5 +13,5 @@ CORS(app)
 
 app.config["REDIS_URL"] = "redis://localhost"
 app.register_blueprint(sse, url_prefix='/api/stream')
-app.register_blueprint(blockchain, url_prefix='/api/blockchain')
+app.register_blueprint(blueprint, url_prefix='/api/blockchain')
 app.register_blueprint(robot, url_prefix='/api/robot')
